a0bdb98498a60dceb37263b2f36e999f50452dea
t0r3x <mjc.vgulik@gmail.com>
2026-05-26 22:01:01 +0200

    Confirm dialogs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,11 @@
   <div class="bg-glow-1"></div>
   <div class="bg-glow-2"></div>
   <ToastContainer />
+  <ConfirmDialog />
   <router-view />
 </template>
 
 <script setup lang="ts">
 import ToastContainer from '@/components/ui/ToastContainer.vue'
+import ConfirmDialog  from '@/components/ui/ConfirmDialog.vue'
 </script>
